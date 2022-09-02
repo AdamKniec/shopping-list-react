be0ae08ebe48734e6202df7c954f5e756ad2e124
AdamKniec <adam.k.kniec@gmail.com>
2022-09-02 23:59:48 +0200

    test commit

--- src/huskyTest.spec.ts
+++ src/huskyTest.spec.ts
@@ -1,6 +1,9 @@
 export {};
 describe("Whatever", () => {
   it("shpould fail", () => {
-    expect(2).toEqual(5);
+    expect(2).toEqual(2);
+  });
+  it("shpould fail", () => {
+    expect(2).toEqual(2);
   });
 });
